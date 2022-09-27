8928298424cb79c1e2fd4a1624b16fe20d79edd8
NDCLRobotics <roboticsndcl@gmail.com>
2022-09-27 16:46:09 -0400

    hardcoding lmao

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlayTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlayTeleOp.java
@@ -28,7 +28,7 @@ public class PowerPlayTeleOp extends OpMode
     private boolean powerSwitching = false;
 
     // Pull the date of the file (to update the date of telemetry)
-    File file = new File("src/main/java/org/firstinspires/ftc/teamcode/PowerPlayTeleOp.java");
+    File file = new File("C:\\Users\\robotics1\\Desktop\\2023-PowerPlay\\TeamCode\\src\\main\\java\\org\\firstinspires\\ftc\\teamcode\\PowerPlayTeleOp.java");
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
     @Override

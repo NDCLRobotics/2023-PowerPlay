ef2e4a740afabf7d836701ff13730903595050ef
NDCLRobotics <roboticsndcl@gmail.com>
2022-10-19 16:41:11 -0400

    Adjust values

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SecondRobotEncoderTest.java
@@ -0,0 +1,103 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.os.Environment;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Stack;
+
+@TeleOp(name="Second Robot Encoder Test", group="Interactive Opmode")
+
+public class SecondRobotEncoderTest extends OpMode
+{
+    // Control Hub
+    private DcMotor frontLeftMotor = null;
+    private DcMotor frontRightMotor = null;
+    private DcMotor backLeftMotor = null;
+    private DcMotor backRightMotor = null;
+
+    @Override
+    public void init ()
+    {
+        // Initialize connection to motors
+        frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+
+        // Set direction to the motors (may need to change depending on orientation of robot)
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Reset the encoder count - intialize to 0
+        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        // Send telemetry to the robot
+        telemetry.addLine("Working");
+        // telemetry.addData("Last updated",sdf.format(file.lastModified()));
+    }
+
+    @Override
+    public void init_loop ()
+    {
+        // pls do
+    }
+
+    @Override
+    public void loop ()
+    {
+        // Reset encoder positions
+        if (gamepad1.a)
+        {
+            frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        }
+
+        if (gamepad1.b)
+        {
+            frontLeftMotor.setTargetPosition(500);
+            frontRightMotor.setTargetPosition(500);
+            backLeftMotor.setTargetPosition(500);
+            backRightMotor.setTargetPosition(500);
+
+            frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            frontLeftMotor.setPower(0.8);
+            frontRightMotor.setPower(0.8);
+            backLeftMotor.setPower(0.8);
+            backRightMotor.setPower(0.8);
+        }
+
+        // Output telemetry
+        telemetry.addData("Front Left", frontLeftMotor.getCurrentPosition());
+        telemetry.addData("Front Right", frontRightMotor.getCurrentPosition());
+        telemetry.addData("Back Left", backLeftMotor.getCurrentPosition());
+        telemetry.addData("Back Right", backRightMotor.getCurrentPosition());
+    }
+
+    @Override
+    public void stop ()
+    {
+        // I don't know how many years in a row I have to reiterate this, pls do
+        frontLeftMotor.setPower(0.0);
+        frontRightMotor.setPower(0.0);
+        backLeftMotor.setPower(0.0);
+        backRightMotor.setPower(0.0);
+    }
+}

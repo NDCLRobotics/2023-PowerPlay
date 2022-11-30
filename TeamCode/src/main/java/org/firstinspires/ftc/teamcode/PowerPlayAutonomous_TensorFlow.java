fce64f5a0205698c1ea713d8c9487d694eb0bbf3
NDCLRobotics <roboticsndcl@gmail.com>
2022-11-30 15:40:47 -0500

    Added bok.tflite

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlayAutonomous_TensorFlow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlayAutonomous_TensorFlow.java
@@ -63,13 +63,12 @@ public class PowerPlayAutonomous_TensorFlow extends LinearOpMode {
      * has been downloaded to the Robot Controller's SD FLASH memory, it must to be loaded using loadModelFromFile()
      * Here we assume it's an Asset.    Also see method initTfod() below .
      */
-    private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";
+    private static final String TFOD_MODEL_ASSET = "bok.tflite";
     // private static final String TFOD_MODEL_FILE  = "/sdcard/FIRST/tflitemodels/CustomTeamModel.tflite";
 
-    private static final String[] LABELS = {
-      "1 Bolt",
-      "2 Bulb",
-      "3 Panel"
+    private static final String[] LABELS =
+    {
+      "Green", "Pink", "Yellow"
     };
 
     /*

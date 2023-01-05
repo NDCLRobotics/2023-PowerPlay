ac7e7f4bbc128632b06a263c4a2ec5d875331c46
NDCLRobotics <roboticsndcl@gmail.com>
2023-01-05 16:49:18 -0500

    reduce pan

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlayAutonomous_TensorFlow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlayAutonomous_TensorFlow.java
@@ -425,10 +425,10 @@ public class PowerPlayAutonomous_TensorFlow extends LinearOpMode {
                         {
 
 
-                            frontLeftMotor.setTargetPosition(-650);
-                            frontRightMotor.setTargetPosition(650);
-                            backLeftMotor.setTargetPosition(650);
-                            backRightMotor.setTargetPosition(-650);
+                            frontLeftMotor.setTargetPosition(-630);
+                            frontRightMotor.setTargetPosition(630);
+                            backLeftMotor.setTargetPosition(630);
+                            backRightMotor.setTargetPosition(-630);
 
                             frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                             frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -441,7 +441,7 @@ public class PowerPlayAutonomous_TensorFlow extends LinearOpMode {
                             backLeftMotor.setPower(0.32);
                             backRightMotor.setPower(-0.32);
 
-                            if (frontLeftMotor.getCurrentPosition() < -645 && frontRightMotor.getCurrentPosition() > 645)
+                            if (frontLeftMotor.getCurrentPosition() < -625 && frontRightMotor.getCurrentPosition() > 625)
                             {
                                 frontLeftMotor.setPower(0.0);
                                 frontRightMotor.setPower(0.0);
